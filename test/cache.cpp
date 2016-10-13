3c0d6cbe741f70418c81a2f2d7bb456af1a2d7c1
Alexander Batischev <eual.jp@gmail.com>
2016-10-13 20:45:54 +0300

    Test cache::clean_old_articles

--- test/cache.cpp
+++ test/cache.cpp
@@ -42,3 +42,55 @@ TEST_CASE("cache behaves correctly") {
 
 	::unlink("test-cache.db");
 }
+
+TEST_CASE("Cleaning old articles works") {
+	/* Populating the cache file with some items. */
+	configcontainer * cfg = nullptr;
+	cache * rsscache = nullptr;
+
+	cfg = new configcontainer();
+	rsscache = new cache("test-cache.db", cfg);
+	rss_parser * parser = new rss_parser(
+			"file://data/rss.xml", rsscache, cfg, NULL);
+	std::shared_ptr<rss_feed> feed = parser->parse();
+
+	/* Adding a fresh item that won't be deleted. If it survives the test, we
+	 * will know that "keep-articles-days" really deletes the old articles
+	 * *only* and not the whole database. */
+	auto item = std::make_shared<rss_item>(rsscache);
+	item->set_title("Test item");
+	item->set_link("http://example.com/item");
+	item->set_guid("http://example.com/item");
+	item->set_author("Newsbeuter Testsuite");
+	item->set_description("");
+	item->set_pubDate(time(NULL)); // current time
+	item->set_unread(true);
+	feed->add_item(item);
+
+	rsscache->externalize_rssfeed(feed, false);
+
+	/* Simulating a restart of Newsbeuter. */
+	delete parser;
+	delete rsscache;
+	delete cfg;
+
+	/* Setting "keep-articles-days" to non-zero value to trigger
+	 * cache::clean_old_articles().
+	 *
+	 * The value of 42 days is sufficient because the items in the test feed
+	 * are dating back to 2006. */
+	cfg = new configcontainer();
+	cfg->set_configvalue("keep-articles-days", "42");
+	rsscache = new cache("test-cache.db", cfg);
+	rss_ignores * ign = new rss_ignores();
+	feed = rsscache->internalize_rssfeed("file://data/rss.xml", ign);
+
+	/* The important part: old articles should be gone, new one remains. */
+	REQUIRE(feed->items().size() == 1);
+
+	/* Cleanup. */
+	delete ign;
+	delete rsscache;
+	delete cfg;
+	::unlink("test-cache.db");
+}
